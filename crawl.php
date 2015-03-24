0c3242b80da2f3b0beed07ebaf8366e2a29daee1
chenxi10 <chenxi10@chenxi10.staff.sina.com.cn>
2015-03-24 21:16:59 +0800

    netease mobile

--- crawl.php
+++ crawl.php
@@ -9,14 +9,50 @@ foreach (SeverityConf::$pages as $key => $value) {
     echo "********************************************************************\n";
     echo "$key\n";
     echo "********************************************************************\n";
+
     phpQuery::newDocumentFile($value['url']);
-    foreach ($value['grades'] as $css => $grade) {
-        $htmlpiece = pq($css)->html();
-        $urls = parse_array($htmlpiece, "<a", "</a>");
-        foreach ($urls as $atag) {
-            echo iconv('gbk', 'UTF-8', $atag) . "\n";
+
+    foreach ($value['rules'] as $rule) {
+        /**
+         * 选取含有url的html片段
+         */
+        $htmlpiece = pq($rule['cssSelector'])->htmlOuter();
+//        echo iconv('gbk', 'UTF-8', $htmlpiece) . "\n";
+
+        /**
+         * 根据包含a标签的祖先标签进行解析
+         */
+        $nodes = parse_array($htmlpiece, '<' . $rule['atagInclude'], '</' . $rule['atagInclude'] . '>');
+
+        foreach ($nodes as $node) {
+//            echo iconv('gbk', 'UTF-8', $node) . "\n";
+            /**
+             * 解析a标签
+             */
+            $atagArray = parse_array($node, '<a', '</a>');
+
+            foreach ($atagArray as $atag) {
+                $saveString = '';
+//                echo iconv('gbk', 'UTF-8', $atag) . "\n";
+
+                /**
+                 * 取url
+                 */
+                $saveString.= get_attribute($atag, "href");
 //            echo iconv('gbk', 'UTF-8', return_between($atag, '>', '<', EXCL)) . "\n";
-            echo get_attribute($atag, "href") . " " . $grade . "\n";
+
+                /**
+                 * 打分数
+                 */
+                $saveString.="\t" . $rule['score'];
+
+                if ($rule['commentTag']) {
+                    $comment = return_between($node, '<' . $rule['commentTag'] . '>', '</' . $rule['commentTag'] . '>', EXCL);
+                    $saveString.="\t" . $comment;
+                }
+
+                echo $saveString . "\n";
+            }
         }
     }
 }
